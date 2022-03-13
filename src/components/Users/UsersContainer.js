b076ed10fdf11f5cada038905dabcccfb72087d1
Rita Ravinsky <ritaravinsky@gmail.com>
2022-03-13 15:05:17 +0200

    refactoring of UsersContainer.js

--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -1,39 +1,89 @@
+import React from "react";
 import { connect } from "react-redux";
 import {
   followActionCreator,
   setUsersActionCreator,
   unfollowActionCreator,
   setCurrentPageActionCreator,
-  setTotalUsersCountActionCreator
+  setTotalUsersCountActionCreator,
 } from "../../redux/users-reducer";
-import Users from './Users';
+import Users from "./Users";
+import * as axios from "axios";
+
+class UsersContainer extends React.Component {
+  componentDidMount() {
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${this.props.currentPage}`
+      )
+      .then((res) => {
+        this.props.onSetUsers(res.data.items);
+        this.props.setTotalUsersCount(res.data.totalCount);
+        console.log("totalCount: " + res.data.totalCount);
+      });
+  }
+  handlePageChange = (pageNum) => {
+    // change current number
+    this.props.onPageChange(pageNum);
+    // ajax call
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${pageNum}`
+      )
+      .then((res) => {
+        this.props.onSetUsers(res.data.items);
+      });
+  };
+
+  render() {
+    const {
+      users,
+      onUnfollow,
+      onFollow,
+      totalUsersCount,
+      pageSize,
+      currentPage,
+    } = this.props;
+    return (
+      <Users
+        users={users}
+        onUnfollow={onUnfollow}
+        onFollow={onFollow}
+        totalUsersCount={totalUsersCount}
+        pageSize={pageSize}
+        currentPage={currentPage}
+        handlePageChange={this.handlePageChange}
+      />
+    );
+  }
+}
 
 const mapStateToProps = (state) => {
-    return {
-      users: state.usersPage.users,
-      pageSize: state.usersPage.pageSize,
-      totalUsersCount: state.usersPage.totalUsersCount,
-      currentPage: state.usersPage.currentPage,
-    };
-}
+  return {
+    users: state.usersPage.users,
+    pageSize: state.usersPage.pageSize,
+    totalUsersCount: state.usersPage.totalUsersCount,
+    currentPage: state.usersPage.currentPage,
+  };
+};
 const mapDispatchToProps = (dispatch) => {
-    return {
-      onFollow: (userId) => {
-        dispatch(followActionCreator(userId));
-      },
-      onUnfollow: (userId) => {
-        dispatch(unfollowActionCreator(userId));
-      },
-      onSetUsers: (users) => {
-        dispatch(setUsersActionCreator(users));
-      },
-      onPageChange: (current) => {
-        dispatch(setCurrentPageActionCreator(current));
-      },
-      setTotalUsersCount: (count) => {
-          dispatch(setTotalUsersCountActionCreator(count))
-      }
-    };
-}
+  return {
+    onFollow: (userId) => {
+      dispatch(followActionCreator(userId));
+    },
+    onUnfollow: (userId) => {
+      dispatch(unfollowActionCreator(userId));
+    },
+    onSetUsers: (users) => {
+      dispatch(setUsersActionCreator(users));
+    },
+    onPageChange: (current) => {
+      dispatch(setCurrentPageActionCreator(current));
+    },
+    setTotalUsersCount: (count) => {
+      dispatch(setTotalUsersCountActionCreator(count));
+    },
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
